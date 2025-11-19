0fa97d218947f79be20d73ff2bf80702b9af66a1
Norran Yu <norran.yu@gmail.com>
2025-11-19 14:43:20 -0500

    reformatted graph

--- nonLinear_regression.py
+++ nonLinear_regression.py
@@ -21,9 +21,9 @@ plt.scatter(X, Y, label='Original Data', color='blue', marker='o', s=100)
 
 plt.plot(X_fit, Y_fit, label=f'Fit: y = {A0_fit:.4f}$e^{{-{k_fit:.4f}x}}$', color='red', linestyle='--')
 
-plt.title('Exponential Decay Fit: Maximum lambda Before Underfitting vs. Neurons per Layer', fontsize=14)
+plt.title('Maximum $\lambda$ Before Underfitting vs. Neurons per Layer', fontsize=14)
 plt.xlabel('Neurons per Layer', fontsize=12)
-plt.ylabel('Maximum lambda Before Underfitting', fontsize=12)
+plt.ylabel('Maximum $\lambda$ Before Underfitting', fontsize=12)
 
 plt.legend(fontsize=10)
 plt.grid(True, linestyle=':', alpha=0.6)
